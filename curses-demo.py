6c4d09df69b4053dc813dfc25046e1b06322da94
László Székely-Tóth <notisracer@gmail.com>
2016-11-15 09:52:50 +0100

    successful vertical line

--- curses-demo.py
+++ curses-demo.py
@@ -9,7 +9,9 @@ def main(stdscr):
     while True:
         a = stdscr.getkey()
         stdscr.clear()
+
         stdscr.addstr(a)
+        stdscr.vline(5, 5, curses.ACS_VLINE, 5)
         stdscr.refresh()
     # stdscr.getkey()
 
