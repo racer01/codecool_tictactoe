a82ecf2080f321cb3a9bff2c6404532a56fd4229
László Székely-Tóth <notisracer@gmail.com>
2016-11-16 11:59:20 +0100

    dynamically calc/print table borders

--- curses-demo.py
+++ curses-demo.py
@@ -5,13 +5,31 @@ import curses
 
 
 def main(stdscr):
-    # Clear screen
+
+    sizeX = 10
+    sizeY = 10
+
+    startX = 3
+    startY = 3
+
+    for rowStart in range(startY + 1, (sizeY * 2) + startY - 1, 2):
+        stdscr.hline(rowStart, startX, curses.ACS_HLINE, (sizeX * 4) - 1)
+    for colStart in range(startX + 3, (sizeX * 4) + startX - 1, 4):
+        stdscr.vline(startY, colStart, curses.ACS_VLINE, (sizeY * 2) - 1)
+    for crossY in range(startY + 1, (sizeY * 2) + startY - 1, 2):
+        for crossX in range(startX + 3, (sizeX * 4) + startX - 1, 4):
+            pass
+
+    locations = (((0, 1), (0, 5), (0, 9)),
+                 ((2, 1), (2, 5), (2, 9)),
+                 ((4, 1), (4, 5), (4, 9)))
+
+    stdscr.move(locations[0][1][0], locations[0][1][1])
     while True:
         a = stdscr.getkey()
         stdscr.clear()
 
         stdscr.addstr(a)
-        stdscr.vline(5, 5, curses.ACS_VLINE, 5)
         stdscr.refresh()
     # stdscr.getkey()
 
