143d0c0f8773dd87ff5734a02dd4e543c8528692
László Székely-Tóth <notisracer@gmail.com>
2016-11-16 12:12:36 +0100

    unified data like size, startpoz, to be a single dict instead multiple primitive vars

--- curses-demo.py
+++ curses-demo.py
@@ -6,29 +6,26 @@ import curses
 
 def main(stdscr):
 
-    sizeX = 10
-    sizeY = 10
+    size = {'y': 10, 'x': 10}
+    startPoz = {'y': 2, 'x': 4}
 
-    startX = 3
-    startY = 3
-
-    for rowStart in range(startY + 1, (sizeY * 2) + startY - 1, 2):
-        stdscr.hline(rowStart, startX, curses.ACS_HLINE, (sizeX * 4) - 1)
-    for colStart in range(startX + 3, (sizeX * 4) + startX - 1, 4):
-        stdscr.vline(startY, colStart, curses.ACS_VLINE, (sizeY * 2) - 1)
-    for crossY in range(startY + 1, (sizeY * 2) + startY - 1, 2):
-        for crossX in range(startX + 3, (sizeX * 4) + startX - 1, 4):
+    for rowStart in range(startPoz['y'] + 1, (size['y'] * 2) + startPoz['y'] - 1, 2):
+        stdscr.hline(rowStart, startPoz['x'], curses.ACS_HLINE, (size['x'] * 4) - 1)
+    for colStart in range(startPoz['x'] + 3, (size['x'] * 4) + startPoz['x'] - 1, 4):
+        stdscr.vline(startPoz['y'], colStart, curses.ACS_VLINE, (size['y'] * 2) - 1)
+    for crossY in range(startPoz['y'] + 1, (size['y'] * 2) + startPoz['y'] - 1, 2):
+        for crossX in range(startPoz['x'] + 3, (size['x'] * 4) + startPoz['x'] - 1, 4):
             stdscr.addch(crossY, crossX, curses.ACS_PLUS)
 
     # (y, x)
     locations = []
-    for y in range(sizeY):
+    for y in range(size['y']):
         row = []
-        for x in range(sizeX):
-            row.append([(y * 2) + startY, ((x * 4) + 1) + startX])
+        for x in range(size['x']):
+            row.append({'y': (y * 2) + startPoz['y'], 'x': ((x * 4) + 1) + startPoz['x']})
         locations.append(row)
 
-    stdscr.move(locations[0][1][0], locations[0][1][1])
+    stdscr.move(locations[0][1]['y'], locations[0][1]['x'])
     while True:
         a = stdscr.getkey()
         stdscr.clear()
