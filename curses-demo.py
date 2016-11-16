58c7e520e3e7e56f1ac1eefdcd5a70355b27c978
László Székely-Tóth <notisracer@gmail.com>
2016-11-16 13:42:58 +0100

    implemented cursor control

--- curses-demo.py
+++ curses-demo.py
@@ -4,11 +4,17 @@ from curses import wrapper
 import curses
 
 
-def main(stdscr):
+def calc_step_locations(size, startPoz):
+    locs = []
+    for y in range(size['y']):
+        row = []
+        for x in range(size['x']):
+            row.append({'y': (y * 2) + startPoz['y'], 'x': ((x * 4) + 1) + startPoz['x']})
+        locs.append(row)
+    return locs
 
-    size = {'y': 10, 'x': 10}
-    startPoz = {'y': 2, 'x': 4}
 
+def print_table(stdscr, size, startPoz):
     for rowStart in range(startPoz['y'] + 1, (size['y'] * 2) + startPoz['y'] - 1, 2):
         stdscr.hline(rowStart, startPoz['x'], curses.ACS_HLINE, (size['x'] * 4) - 1)
     for colStart in range(startPoz['x'] + 3, (size['x'] * 4) + startPoz['x'] - 1, 4):
@@ -17,20 +23,40 @@ def main(stdscr):
         for crossX in range(startPoz['x'] + 3, (size['x'] * 4) + startPoz['x'] - 1, 4):
             stdscr.addch(crossY, crossX, curses.ACS_PLUS)
 
+
+def main(stdscr):
+
+    size = {'y': 10, 'x': 10}
+    startPoz = {'y': 2, 'x': 4}
+    currentPoz = {'y': 0, 'x': 0}
+
+    print_table(stdscr, size, startPoz)
     # (y, x)
-    locations = []
-    for y in range(size['y']):
-        row = []
-        for x in range(size['x']):
-            row.append({'y': (y * 2) + startPoz['y'], 'x': ((x * 4) + 1) + startPoz['x']})
-        locations.append(row)
+    locations = calc_step_locations(size, startPoz)
 
-    stdscr.move(locations[0][1]['y'], locations[0][1]['x'])
+    stdscr.move(locations[currentPoz['y']][currentPoz['x']]['y'], locations[currentPoz['y']][currentPoz['x']]['x'])
     while True:
-        a = stdscr.getkey()
-        stdscr.clear()
+        k = stdscr.getkey()
+        # stdscr.clear()
+        # stdscr.addstr(k)
+        if k == "KEY_DOWN":
+            if currentPoz['y'] < size['y'] - 1:
+                currentPoz['y'] += 1
+        elif k == "KEY_UP":
+            if currentPoz['y'] > 1 - 1:
+                currentPoz['y'] -= 1
+        elif k == "KEY_LEFT":
+            if currentPoz['x'] > 1 - 1:
+                currentPoz['x'] -= 1
+        elif k == "KEY_RIGHT":
+            if currentPoz['x'] < size['x'] - 1:
+                currentPoz['x'] += 1
+        else:
+            continue
+        stdscr.move(0, 0)
+        stdscr.addstr(str(currentPoz))
+        stdscr.move(locations[currentPoz['y']][currentPoz['x']]['y'], locations[currentPoz['y']][currentPoz['x']]['x'])
 
-        stdscr.addstr(a)
         stdscr.refresh()
     # stdscr.getkey()
 
